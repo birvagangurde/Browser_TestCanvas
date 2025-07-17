acc0a62713cfbbf6217629f896938f3bc051008c
birvagangurde <gangurdebirva@gmail.com>
2025-07-17 23:48:39 +0530

    understand browser

---
+++ 5.understand_browser.py
@@ -0,0 +1,11 @@
+# Chapter 32 - Selenium Course
+import time
+
+from selenium import webdriver
+# Chrome driver service
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com")
+
+
+
+time.sleep(2)
