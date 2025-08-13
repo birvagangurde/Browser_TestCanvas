21237a7ef41ed86542dbb764d556acc1325c3ee3
birvagangurde <gangurdebirva@gmail.com>
2025-08-13 23:49:56 +0530

    Starting Pytest

--- 3.E2E_Pytest/pytestDemo/conftest.py
+++ 3.E2E_Pytest/pytestDemo/conftest.py
@@ -6,3 +6,14 @@ def setup():
     print("I will be executing first")
     yield
     print("I willl execute in fixtureDemo method")
+
+
+@pytest.fixture()
+def dataLoad():
+    print("user profile data is being created")
+    return ["Rahul", "Shetty", "rahulshettyacademy.com"]
+
+
+@pytest.fixture(params=[("chrome", "Rahul", "shetty"), ("Firefox", "Rahul"), ("IE", "SS")])
+def crossBrowser(request):
+    return request.param
--- 3.E2E_Pytest/pytestDemo/test_demo1.py
+++ 3.E2E_Pytest/pytestDemo/test_demo1.py
@@ -1,12 +1,14 @@
+import pytest
 
 
 def test_firstProgram():
     print("Hello")
 
+
 # The below program overwrites the first program
 # def test_firstProgram():
 #     print("Good Morning")
 
-
 def test_SecondGreetCreditCard():
     print("Good Morning")
+
